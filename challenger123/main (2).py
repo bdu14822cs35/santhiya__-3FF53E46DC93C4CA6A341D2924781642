366ff9bb95c1c4a853603da5200e1bba45076942
bdu14822cs35 <144336867+bdu14822cs35@users.noreply.github.com>
2023-10-18 18:30:54 +0530

    Add files via upload

---
+++ challenger123/main (2).py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number"))
+if(a%400==0)and(a%200==0):
+  print ("it is a century year and also a leap year")
+elif(a%4==0)and(a%100!=0):
+  print("it is not a century year but it is leap year")
+else:
+  print("it is not a leap year")
